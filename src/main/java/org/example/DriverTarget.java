4ed6d2e1a542df42c49931e2800bf00592d6e3a2
moolya-arun <arun.kumaar@moolya.com>
2023-03-30 12:43:39 +0530

    BasicActions.java

--- src/main/java/org/example/DriverTarget.java
+++ src/main/java/org/example/DriverTarget.java
@@ -17,15 +17,15 @@ public class DriverTarget {
         capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
         capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION,"12");
         capabilities.setCapability(MobileCapabilityType.UDID,"emulator-5554");
-    // capabilities.setCapability("app","C:/Users/arund/Downloads/calculatrice.apk");
+    capabilities.setCapability(MobileCapabilityType.APP,"C:/Users/arund/Downloads/ApiDemos-debug.apk");
 
-        capabilities.setCapability("appPackage","com.bak.mnr.calculatrice");
+       // capabilities.setCapability("appPackage","com.bak.mnr.calculatrice");
 
-        capabilities.setCapability("appActivity", "MainActivity");
-        capabilities.setCapability("avd","pixel");
+        //apabilities.setCapability("appActivity", "MainActivity");
+        capabilities.setCapability("avd","pixel3");
         capabilities.setCapability("avd.timeout",280);
 
-    URL url = new URL("http://0.0.0.0:4723/wd/hub");
+    URL url = new URL("http://127.0.0.1:4723/wd/hub");
     AndroidDriver<MobileElement> driver = new AndroidDriver<>(url,capabilities);
 return driver;
 
